5542afb91032eee2332311bc04d9902a8b2e9f04
eligocs <38901899+eligocs@users.noreply.github.com>
2023-10-17 18:21:49 +0530

    Seeder changes

--- database/migrations/software/2023_10_05_123026_tblrelated_items.php
+++ database/migrations/software/2023_10_05_123026_tblrelated_items.php
@@ -30,17 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::create('tblrelated_items', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('rel_id');
-            $table->string('rel_type', 30);
-            $table->integer('item_id');
-            
-            // Add foreign key constraints if necessary
-            // //$table->foreign('rel_id')->references('id')->on('related_table');
-            // //$table->foreign('item_id')->references('id')->on('item_table');
-            
-            $table->timestamps();
-        });
+        Schema::dropIfExists('tblrelated_items');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(DummyDataSeeder::class);
+        // $this->call(DummyDataSeeder::class);
         $this->call(DummyDataSeederTwo::class);
     }
 }
